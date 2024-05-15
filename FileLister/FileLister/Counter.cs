c5695e80e43abaa691a3363c3c97e8a5c2436b92
Aakash Kishan <142676432+aakash-kishan@users.noreply.github.com>
2024-05-15 12:19:16 +0530

    Add files via upload

---
+++ FileLister/FileLister/Counter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace FileLister
+{
+    public abstract class Counter
+    {
+        public abstract void Display();
+    }
+
+    public class FileDisplay : Counter
+    {
+        private string path;
+
+        public string Paths
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Enter the keyword: ");
+            string kword = Console.ReadLine();
+            string[] files = Directory.GetFiles(path, "*.txt");
+            
+            
+            
+            foreach (string file in files)
+            {
+                bool keywordFound = SearchKeywordInFile(file, kword);
+                
+
+                if (keywordFound)
+                {
+                    string fileName = Path.GetFileName(path);
+                    Console.WriteLine($"File '{fileName}' contains the keyword '{kword}'");
+
+                    
+                }
+
+                
+            }
+           
+            
+            
+
+        }
+
+        private bool SearchKeywordInFile(string filePath, string searchKeyword)
+        {
+            string content = File.ReadAllText(filePath);
+
+            int v = ContainsCaseInsensitive(content, searchKeyword);
+            
+            if (v>0)
+            {
+                Console.WriteLine($"The no. of counts: {v}");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        private int ContainsCaseInsensitive(string content, string searchKeyword)
+        {
+            int count = 0;
+            for (int i = 0; i <= content.Length - searchKeyword.Length; i++)
+            {
+                bool match = true;
+                
+
+                
+
+                for (int j = 0; j < searchKeyword.Length; j++)
+                {
+                    if ((content[i + j]) != (searchKeyword[j]))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                
+                if (match)
+                {
+
+                    count++;
+                    
+                    
+                }
+                
+
+            }
+
+            return count;
+        }
+        
+
+        
+    }
+}
---
+++ FileLister/FileLister/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileDisplay fd = new FileDisplay();
+            fd.Paths = "C:\\Users\\DELL\\source\\repos\\FileLister\\FileLister\\testfiles\\";
+            fd.Display();
+            ;
+
+        }
+    }
+}
